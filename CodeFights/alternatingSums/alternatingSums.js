86939aab80d8a663ffad82dfdd4ab438dd37362e
Aaron <aaronmassey45@gmail.com>
2018-06-23 13:06:52 -0500

    Added alternatingSums

---
+++ CodeFights/alternatingSums/alternatingSums.js
@@ -0,0 +1,19 @@
+function alternatingSums(a) {
+  if (a.length === 1) return [...a, 0];
+  if (a.length === 2) return a;
+
+  const teams = { a: [], b: [] };
+  a.forEach((weight, i) => {
+    i % 2 === 0 ? teams.a.push(weight) : teams.b.push(weight);
+  });
+
+  Object.keys(teams).forEach(key => {
+    teams[key] = teams[key].reduce((acc, val) => acc + val);
+  });
+
+  return Object.values(teams);
+}
+
+console.log(alternatingSums([50, 60, 60, 45, 70]));
+console.log(alternatingSums([100, 50]));
+console.log(alternatingSums([80]));
