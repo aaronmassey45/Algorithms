8738f33e7806e7f16d0aaaefe052596ace75e318
Aaron <aaronmassey45@gmail.com>
2018-07-10 12:40:22 -0500

    Added arrayMaximalAdjacentDifference

---
+++ CodeSignal/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
@@ -0,0 +1,15 @@
+const arrayMaximalAdjacentDifference = inputArray => {
+  let max = 0;
+  inputArray.forEach((num, i) => {
+    if (!!inputArray[i + 1]) {
+      let difference = num - inputArray[i + 1];
+      if (difference < 0) difference *= -1;
+      if (difference > max) max = difference;
+    }
+  });
+  return max;
+};
+
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([-1, 4, 10, 3, -2]));
+console.log(arrayMaximalAdjacentDifference([-1, 1, -3, -4]));
