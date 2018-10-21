dc5b6592606bd585c5062d11c6f9e2468a73c08b
Aaron <aaronmassey45@gmail.com>
2018-10-21 11:07:20 -0500

    Updated algorithm. Now only goes through string once

--- CodeSignal/alphabeticShift/index.js
+++ CodeSignal/alphabeticShift/index.js
@@ -1,12 +1,18 @@
 function alphabeticShift(inputString) {
-  return inputString
-    .split('')
-    .map(char => {
-      const newChar =
-        char.charCodeAt(0) + 1 < 123 ? char.charCodeAt(0) + 1 : 97;
-      return String.fromCharCode(newChar);
-    })
-    .join('');
+  let str = '';
+  for (let char of inputString) {
+    const newChar = char.charCodeAt() + 1 < 123 ? char.charCodeAt() + 1 : 97;
+    str += String.fromCharCode(newChar);
+  }
+  return str;
+  // return inputString
+  //   .split('')
+  //   .map(char => {
+  //     const newChar =
+  //       char.charCodeAt(0) + 1 < 123 ? char.charCodeAt(0) + 1 : 97;
+  //     return String.fromCharCode(newChar);
+  //   })
+  //   .join('');
 }
 
 module.exports = alphabeticShift;
